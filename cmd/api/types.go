14cfc6ffc4d67232f82239e7cb43d6bdf83a7119
CorreyL <correylim@gmail.com>
2024-05-03 19:47:49 -0700

    Add Military and GrandTemple types

--- cmd/api/types.go
+++ cmd/api/types.go
@@ -7,3 +7,9 @@ type Civilian struct {
 	Six	int `json:"6"`
 	Seven	int `json:"7"`
 }
+
+// TODO: See if there's a way to restrict values to 0 | 2 | 5 | 10
+type Military int
+
+// TODO: See if there's a way to restrict values to 0 | 5 | 12 | 21
+type GrandTemple int
