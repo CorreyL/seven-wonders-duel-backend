6876c221ffe54e3ebb7d55b7f3b1a1afe8a2efa0
CorreyL <correylim@gmail.com>
2024-05-03 20:00:44 -0700

    Add code to return dummy JSON data using Civilian

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
+// TODO: Remove function, this is just to test that gin works
+func getCivilian(c *gin.Context) {
+	var civ Civilian = Civilian{
+		Three: 1,
+	}
+	c.IndentedJSON(http.StatusOK, civ);
+}
 
 func main() {
-	fmt.Print("Hello World")
+	router := gin.Default()
+	router.GET("/civilian", getCivilian)
+
+	const host string = "localhost:8080";
+
+	router.Run(host)
+	fmt.Printf("Running on %q\n", host)
 }
